223d6837f0e80190ce874c7283a19e365339ca01
Alexander Rauchfuss <araucfuss@dancingtortoise.com>
2010-03-13 21:41:21 -0500

    Fixed build settings so that SC would run properly on Leopard systems.

--- Preferences/GeneralPrefController.h
+++ Preferences/GeneralPrefController.h
@@ -1,9 +1,11 @@
 #import <Cocoa/Cocoa.h>
 #import "SS_PreferencePaneProtocol.h"
 
+
 @interface GeneralPrefController : NSObject <SS_PreferencePaneProtocol> 
 {
     IBOutlet NSView * prefsView;
 }
+
 
 @end
