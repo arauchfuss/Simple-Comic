100dcb964d875df8549fc255088d52a5a9427986
C.W. Betts <computers57@hotmail.com>
2016-05-19 14:22:02 -0600

    Trim roundedRectWithCornerRadius down to one Objective-C call (it was added in 10.5; we're targeting 10.9). Rewrite centerPointOfRect to use NSMid? functions.

--- Classes/Helpers/TSSTImageUtilities.m
+++ Classes/Helpers/TSSTImageUtilities.m
@@ -130,25 +130,15 @@ NSImage * imageScaledToSizeFromImage(NSSize size, NSImage * image)
 NSPoint centerPointOfRect(NSRect rect)
 {
     NSPoint point;
-    point.x = NSMinX(rect) + NSWidth(rect) / 2;
-    point.y = NSMinY(rect) + NSHeight(rect) / 2;
+    point.x = NSMidX(rect);
+    point.y = NSMidY(rect);
     return point;
 }
 
 
 NSBezierPath * roundedRectWithCornerRadius(NSRect aRect, CGFloat radius)
 {
-    NSBezierPath * path = [NSBezierPath bezierPath];
-    radius = MIN(radius, 0.5f * MIN(NSWidth(aRect), NSHeight(aRect)));
-    NSRect rect = NSInsetRect(aRect, radius, radius);
-    
-    [path appendBezierPathWithArcWithCenter: NSMakePoint( NSMinX(rect), NSMinY(rect)) radius: radius startAngle: 180.0 endAngle: 270.0];
-    [path appendBezierPathWithArcWithCenter: NSMakePoint( NSMaxX(rect), NSMinY(rect)) radius: radius startAngle: 270.0 endAngle: 360.0];
-    [path appendBezierPathWithArcWithCenter: NSMakePoint( NSMaxX(rect), NSMaxY(rect)) radius: radius startAngle:  0.0 endAngle: 90.0];
-    [path appendBezierPathWithArcWithCenter: NSMakePoint( NSMinX(rect), NSMaxY(rect)) radius: radius startAngle: 90.0 endAngle: 180.0];
-
-    [path closePath];
-    return path;	
+	return [NSBezierPath bezierPathWithRoundedRect: aRect xRadius: radius yRadius: radius];
 }
 
 
