0e20a595f64a554afe577d748ae72921449e5c47
C.W. Betts <computers57@hotmail.com>
2017-04-21 14:24:59 -0600

    Mark some more stuff as documentation comments.

--- Classes/Session/TSSTPageView.h
+++ Classes/Session/TSSTPageView.h
@@ -16,12 +16,6 @@
 	Simple Comic
 	TSSTPageView.h
 */
-/*
-	Composites one or two images to the screen, making sure that they
-	are horizontally alligned.
-	None of the logic involving the aspect ratios of the images is
-	in this class.
-*/
 
 #import <Cocoa/Cocoa.h>
 #import <QuartzCore/QuartzCore.h>
@@ -64,54 +58,60 @@ typedef struct {
 
 
 
+/*!
+	Composites one or two images to the screen, making sure that they
+	are horizontally alligned.
+	None of the logic involving the aspect ratios of the images is
+	in this class.
+ */
 @interface TSSTPageView : NSView
 
 @property (nonatomic, assign) NSInteger rotation;
 @property (weak) IBOutlet TSSTSessionWindowController * sessionController;
 
 
-/*  This is where it all begins sets the two pages.
+/*!  This is where it all begins sets the two pages.
     Starts any animations
     Calls resize view
     Calls correctViewPoint */
 - (void)setFirstPage:(nullable NSImage *)first secondPageImage:(nullable NSImage *)second;
 
 
-/*  Finds the size of the pages that are to be renedered.
+/*!  Finds the size of the pages that are to be renedered.
     This includes minor scaling to equalize the height of facing pages. */
 - (NSSize)combinedImageSizeForZoom:(CGFloat)level;
 
 
-/*  If the view are is larger than the display area
+/*!  If the view are is larger than the display area
     then drag scrolling is available. */
 @property (readonly) BOOL dragIsPossible;
 
 
-/*  This is used by the image loupe.  Grabs the portion of the pages
+/*!  This is used by the image loupe.  Grabs the portion of the pages
     displayed that fall within "rect." The origin of the argument
     is centered instead of at the bottom left. */
 - (nullable NSImage *)imageInRect:(NSRect)rect;
 
 
-/*  This is the actual rectangle within which the pages are rendered.
+/*!  This is the actual rectangle within which the pages are rendered.
     Handy to know for various reasons. */
 @property (readonly) NSRect imageBounds;
 
 
-/* This method scrolls the views so that the proper
+/*! This method scrolls the views so that the proper
     corner is displayed if the pages rendered are
     larger than the display area. */
 - (void)correctViewPoint;
 
 
-/*  Resizes the view based on the current scaling method and the
+/*!  Resizes the view based on the current scaling method and the
     selected images. */
 - (void)resizeView;
 
-/* Used to find the click area for page selection */
+/*! Used to find the click area for page selection */
 - (NSRect)pageSelectionRect:(NSInteger)selection;
 
-/*	Concatinates an affine transform to the context.
+/*!	Concatinates an affine transform to the context.
 	This is what enables the page rotation. */
 - (void)rotationTransformWithFrame:(NSRect)rect;
 
