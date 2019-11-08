d0a6e5732b2933c3c8d6bcb3b15d44c282e1cbfc
C.W. Betts <computers57@hotmail.com>
2019-11-07 23:06:34 -0700

    Indent GenerateThumbnailForURL.

--- QuickComic/GenerateThumbnailForURL.m
+++ QuickComic/GenerateThumbnailForURL.m
@@ -39,89 +39,89 @@ typedef NS_ENUM(NSInteger, QLThumbnailIconFlavor)
 OSStatus GenerateThumbnailForURL(void *thisInterface, QLThumbnailRequestRef thumbnail, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options, CGSize maxSize)
 {
 	@autoreleasepool {
-	[NSImageRep registerImageRepClass:[TSSTWebPImageRep class]];
-	NSURL *archiveURL = (__bridge NSURL *)url;
-	NSString * archivePath = [archiveURL path];
+		[NSImageRep registerImageRepClass:[TSSTWebPImageRep class]];
+		NSURL *archiveURL = (__bridge NSURL *)url;
+		NSString * archivePath = [archiveURL path];
 //	NSLog(@"base path %@",archivePath);
-	NSData * imageData = nil;
-	NSString * coverName = [UKXattrMetadataStore stringForKey: SCQuickLookCoverName atPath: archivePath traverseLink: NO error: nil] ?: @"";
+		NSData * imageData = nil;
+		NSString * coverName = [UKXattrMetadataStore stringForKey: SCQuickLookCoverName atPath: archivePath traverseLink: NO error: nil] ?: @"";
 //	NSLog(@"page name %@",coverName);
-	NSString * coverRectString = [UKXattrMetadataStore stringForKey: SCQuickLookCoverRect atPath: archivePath traverseLink: NO error: nil] ?: @"";
+		NSString * coverRectString = [UKXattrMetadataStore stringForKey: SCQuickLookCoverRect atPath: archivePath traverseLink: NO error: nil] ?: @"";
 //	NSLog(@"rect %@",coverRectString);
-	CGRect cropRect = CGRectZero;
-	NSInteger coverIndex;
-	if(![coverName isEqualToString: @""])
-	{
+		CGRect cropRect = CGRectZero;
+		NSInteger coverIndex;
+		if(![coverName isEqualToString: @""])
+		{
 //		NSLog(@"has name");
-		DTPartialArchiveParser * partialArchive = [[DTPartialArchiveParser alloc] initWithURL: archiveURL searchString: coverName];
-		if(![coverRectString isEqualToString: @""])
-		{
-			cropRect = NSRectToCGRect(NSRectFromString(coverRectString));
-		}
-		imageData = [partialArchive searchResult];
-	}
-	else
-	{
-		XADArchive * archive = [[XADArchive alloc] initWithFileURL: archiveURL delegate: nil error: NULL];
-		NSMutableArray * fileList = fileListForArchive(archive);
-		
-		if([fileList count] > 0)
-		{
-			[fileList sortUsingDescriptors: fileSort()];
-			coverName = [fileList.firstObject valueForKey: @"rawName"];
-			coverIndex = [[fileList.firstObject valueForKey: @"index"] integerValue];
-			[UKXattrMetadataStore setString: coverName forKey: SCQuickLookCoverName atPath: archivePath traverseLink: NO error: nil];
-			imageData = [archive contentsOfEntry: coverIndex];
-		}
-	}
-	
-	if (QLThumbnailRequestIsCancelled(thumbnail)) {
-		return kQLReturnNoError;
-	}
-	
-	if(imageData)
-	{
-//		NSLog(@"has data");
-		CGImageSourceRef pageSourceRef = CGImageSourceCreateWithData( (CFDataRef)imageData,  NULL);
-		CGImageRef currentImage = CGImageSourceCreateImageAtIndex(pageSourceRef, 0, NULL);
-		CFRelease(pageSourceRef);
-		CGRect canvasRect;
-		CGRect drawRect;
-		if(CGRectEqualToRect(cropRect, CGRectZero))
-		{
-//			NSLog(@"no crop");
-			canvasRect.size = fitSizeInSize(maxSize, CGSizeMake( CGImageGetWidth(currentImage), CGImageGetHeight(currentImage)));
-			canvasRect.origin = CGPointZero;
-			drawRect = canvasRect;
+			DTPartialArchiveParser * partialArchive = [[DTPartialArchiveParser alloc] initWithURL: archiveURL searchString: coverName];
+			if(![coverRectString isEqualToString: @""])
+			{
+				cropRect = NSRectToCGRect(NSRectFromString(coverRectString));
+			}
+			imageData = [partialArchive searchResult];
 		}
 		else
 		{
-//			NSLog(@"crop");
-			canvasRect.size = fitSizeInSize(maxSize, cropRect.size);
-			CGFloat vertScale = canvasRect.size.height / CGImageGetHeight(currentImage);
-			CGFloat horScale = canvasRect.size.width / CGImageGetWidth(currentImage);
-			drawRect.origin = CGPointMake(-(cropRect.origin.x), -(cropRect.origin.y));
-			drawRect.size = CGSizeMake(cropRect.size.width / horScale, cropRect.size.height / vertScale);
+			XADArchive * archive = [[XADArchive alloc] initWithFileURL: archiveURL delegate: nil error: NULL];
+			NSMutableArray * fileList = fileListForArchive(archive);
+			
+			if([fileList count] > 0)
+			{
+				[fileList sortUsingDescriptors: fileSort()];
+				coverName = [fileList.firstObject valueForKey: @"rawName"];
+				coverIndex = [[fileList.firstObject valueForKey: @"index"] integerValue];
+				[UKXattrMetadataStore setString: coverName forKey: SCQuickLookCoverName atPath: archivePath traverseLink: NO error: nil];
+				imageData = [archive contentsOfEntry: coverIndex];
+			}
 		}
-		
+	
 		if (QLThumbnailRequestIsCancelled(thumbnail)) {
-			CFRelease(currentImage);
 			return kQLReturnNoError;
 		}
 		
-		NSDictionary *properties = @{(__bridge NSString *)kQLThumbnailPropertyIconFlavorKey: @(kQLThumbnailIconBookFlavor)};
-		CGContextRef cgContext = QLThumbnailRequestCreateContext(thumbnail, canvasRect.size, false, (__bridge CFDictionaryRef)(properties));
-		if(cgContext)
+		if(imageData)
 		{
+//		NSLog(@"has data");
+			CGImageSourceRef pageSourceRef = CGImageSourceCreateWithData( (CFDataRef)imageData,  NULL);
+			CGImageRef currentImage = CGImageSourceCreateImageAtIndex(pageSourceRef, 0, NULL);
+			CFRelease(pageSourceRef);
+			CGRect canvasRect;
+			CGRect drawRect;
+			if(CGRectEqualToRect(cropRect, CGRectZero))
+			{
+//			NSLog(@"no crop");
+				canvasRect.size = fitSizeInSize(maxSize, CGSizeMake( CGImageGetWidth(currentImage), CGImageGetHeight(currentImage)));
+				canvasRect.origin = CGPointZero;
+				drawRect = canvasRect;
+			}
+			else
+			{
+//			NSLog(@"crop");
+				canvasRect.size = fitSizeInSize(maxSize, cropRect.size);
+				CGFloat vertScale = canvasRect.size.height / CGImageGetHeight(currentImage);
+				CGFloat horScale = canvasRect.size.width / CGImageGetWidth(currentImage);
+				drawRect.origin = CGPointMake(-(cropRect.origin.x), -(cropRect.origin.y));
+				drawRect.size = CGSizeMake(cropRect.size.width / horScale, cropRect.size.height / vertScale);
+			}
+			
+			if (QLThumbnailRequestIsCancelled(thumbnail)) {
+				CFRelease(currentImage);
+				return kQLReturnNoError;
+			}
+			
+			NSDictionary *properties = @{(__bridge NSString *)kQLThumbnailPropertyIconFlavorKey: @(kQLThumbnailIconBookFlavor)};
+			CGContextRef cgContext = QLThumbnailRequestCreateContext(thumbnail, canvasRect.size, false, (__bridge CFDictionaryRef)(properties));
+			if(cgContext)
+			{
 //			NSLog(@"draw");
-			CGContextDrawImage(cgContext, drawRect, currentImage);
-		}
+				CGContextDrawImage(cgContext, drawRect, currentImage);
+			}
 		
 //		NSLog(@"release");
-		CFRelease(currentImage);
-		QLThumbnailRequestFlushContext(thumbnail, cgContext);
-		CGContextRelease(cgContext);
-	}
+			CFRelease(currentImage);
+			QLThumbnailRequestFlushContext(thumbnail, cgContext);
+			CGContextRelease(cgContext);
+		}
 	
 	}
     return noErr;
