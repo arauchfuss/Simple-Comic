3aa998a5a34e6d69df48344b0b8044b521717b38
sygao <syugao@gmail.com>
2020-06-07 16:41:51 -0700

    Fixes #6 (with caveats)
    - Prevents NSWindow from calling zoom toggle when the window is already zoomed. Added a console message to show when toggle is not called.
    - Precise conditions for initializing an already zoomed window still unknown.

--- Classes/Session/TSSTSessionWindowController.m
+++ Classes/Session/TSSTSessionWindowController.m
@@ -960,8 +960,14 @@ NSString * const TSSTMouseDragNotification = @"SCMouseDragNotification";
 	{
 		newSession = YES;
 		[self setShouldCascadeWindows: YES];
-		NSLog(@"isZoomed = %d", [[self window] isZoomed]);
-//		[[self window] zoom: self];
+		
+		if (![[self window] isZoomed])
+		{
+			[[self window] zoom: self];
+		} else {
+			NSLog(@"Window is already zoomed – zoom not toggled");
+		}
+		
 		[pageView correctViewPoint];
 	}
 }
